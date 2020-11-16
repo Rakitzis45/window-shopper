d73fa697ed6699d2d023994ead32260a9a9a402e
Rakitzis45 <67162811+Rakitzis45@users.noreply.github.com>
2020-11-16 18:23:37 -0500

    created helper methods to see if the user is logged in or if that username is taken already

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,26 @@
 class SessionController < ApplicationController
+    get '/login' do
+        if logged_in?
+    
+            redirect to "/user/#{session[:user_id]}"
+        end
+        erb :'sessions/login'
+    end
 
+    post '/login' do
+        user = User.find_by(username:params[:username])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id.to_s
+            redirect to "/user/#{user.id}"
+        else
+            redirect to '/login'
+        end
+    end
+
+    get '/logout' do
+        session.clear
+        redirect to '/login'
+    end
+
+   
 end
