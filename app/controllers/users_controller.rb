41fe36e1d7a929e4a63a2ce2d425b5942fb70bff
Rakitzis45 <67162811+Rakitzis45@users.noreply.github.com>
2020-11-16 18:24:09 -0500

    update formating

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,39 @@
 class UsersController < ApplicationController
 
-    get "/user" do 
-        "hello World"
-        binding.pry
+    get "/user" do #index
+        "test"
     end
 
-    get '/user/new' do #new
+    get '/signup' do #new
+        
+        if logged_in?
+            redirect to '/user/:id'
+        else
+            erb :'sessions/signup'
+        end
     end
 
     get '/user/:id' do #show
+        if !logged_in?
+            redirect to '/login'
+        end
+        if session[:user_id] == params[:id]
+            @user = User.find_by(id:session[:user_id])
+            
+            erb :'users/homepage'
+        else
+            erb :'sessions/error'
+        end
+        
+        
         
     end
 
-    post '/user/new' do #create 
+    post '/signup' do #create 
+        @user = User.new(params[:user])
+        already_taken?
+        redirct to '/login'
+        
     end
 
     get '/user/:id/edit' do #edit
