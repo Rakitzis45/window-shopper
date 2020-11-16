f9750d470e025939b6ed3bac310adae23c9dccbf
Rakitzis45 <67162811+Rakitzis45@users.noreply.github.com>
2020-11-16 14:04:34 -0500

    removed SecureRandom
    removed Secure Random Until i properly know how to use it.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,25 @@ class ApplicationController < Sinatra::Base
 
   configure do
     enable :sessions
-    set :session_secret, SecureRandom.hex(64) 
+    set :session_secret, "secret" 
     set :public_folder, 'public'
     set :views, 'app/views'
+  end
+  get "/" do
+    erb :welcome
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
     end
 
-
-    get "/" do
-      erb :welcome
+    def already_taken?
+      if User.find_by(username: params[:user][:username]) == nil
+        @user.save
+      else
+        redirect to '/signup'
+      end
     end
-  
-
+  end
 end
