080242036050e24809967364b5a677ccf06cc74d
Rakitzis45 <67162811+Rakitzis45@users.noreply.github.com>
2020-11-17 15:29:58 -0500

    Update lists_controller.rb

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,23 +1,20 @@
 class ListsController < ApplicationController
 
-    get '/lists/new' do
+    get '/lists/new' do #new
         erb :'lists/new'
     end
 
-    post '/lists/new' do
+    post '/lists/new' do #create
         List.create(name:params[:list][:name], user_id: session[:user_id].to_i)
         redirect to "/user/#{session[:user_id]}"
     end
 
-    get '/lists/:id' do
+    get '/lists/:id' do #show
         @user = User.find_by(id:session[:user_id])
-
+        
         if @user.lists.find_by_id(params[:id].to_i) == nil
             erb :'sessions/error'
         else
-        #find a way to check to see if list_id is in @user.lists
-        #if it is continue
-        #else redirect to error page
         @list = List.find_by(id:params[:id])
         erb :'lists/show'
         end
